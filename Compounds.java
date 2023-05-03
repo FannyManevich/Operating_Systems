18974f0f939c110ed384d5944f074682ec0700a6
FannyManevich <89281719+FannyManevich@users.noreply.github.com>
2023-05-03 14:28:48 +0300

    Assignment #2

---
+++ Compounds.java
@@ -0,0 +1,12 @@
+
+class Compounds{
+    private void eat{
+        System.out.println("The cow is eating");
+    }
+    private void drink{
+        System.out.println("The cow is drinking");
+    }
+    private void walk{
+        System.out.println("The cow is walking");
+    }
+}
